0adbfbccdf7461a9eeed20e8a0af1f80f8fdf9b8
kevin12345-ai <1277830267@qq.com>
2025-10-08 19:43:15 +0800

    add new readme file

---
+++ diankongzuoye/week1/helloworld.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{   int num;
+    while(114514)
+    {
+    scanf("%d",&num);
+    if(num==0){
+    printf("helloword\n");
+    }
+    if(num==1){
+    printf("HELLOWORLD\n");
+    }
+    if(num==-1)
+    break;
+    }
+}
